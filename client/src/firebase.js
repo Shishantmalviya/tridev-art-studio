84954c620dc0f74825e283b8f016d3ad83d4e3a7
shishantmalviya <shishantmalviya@gmail.com>
2024-04-22 23:27:24 -0700

    api key removed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyByeVySPW-wPbwIb-cueX2JDyO4s1tQEPs",
-  authDomain: "tridev-art.firebaseapp.com",
-  projectId: "tridev-art",
-  storageBucket: "tridev-art.appspot.com",
-  messagingSenderId: "164963352454",
-  appId: "1:164963352454:web:6f47d9228b45ca5bb2b607",
-  measurementId: "G-1QSS11JR7K"
+  
 };
 
 // Initialize Firebase
